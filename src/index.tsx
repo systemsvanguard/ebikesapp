3605c0f0a07d93200113a47554c141be56aadb4f
Ryan Hunter <36390187+systemsvanguard@users.noreply.github.com>
2025-05-22 21:33:14 -0400

    Added Bootstrap.JS for NavBar
    Added Bootstrap.JS for NavBar

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import stylings
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import './assets/fontawesome/css/fontawesome.min.css';
 import './assets/fontawesome/css/solid.min.css';
